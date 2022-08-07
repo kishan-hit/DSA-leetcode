5df650b83ca5ba6eb2b599a7dd86dab6c60e1a8b
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-08-07 21:14:55 +0530

    Time: 4 ms (28.92%), Space: 6.5 MB (46.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,7 +10,21 @@ public:
         return dp[i][j] = fun(i-1,j,dp) + fun(i,j-1,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return fun(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0)
+                    dp[i][j] = 1;
+                else{
+                    int cnt=0;
+                    if(i-1>=0)
+                        cnt += dp[i-1][j];
+                    if(j-1>=0)
+                        cnt += dp[i][j-1];
+                    dp[i][j] = cnt;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
