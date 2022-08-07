a7bdc89fe7590efaa5d12256c9df447b9e0371d7
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-08-08 01:08:03 +0530

    Time: 63 ms (30.69%), Space: 18.3 MB (48.93%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int fun(int amount,vector<int>&coins,int i,vector<vector<int>>&dp){
-        if(i==0)
-            return (amount%coins[0]==0);
-        if(dp[i][amount]!=-1)
-            return dp[i][amount];
-        if(amount==0)
-            return 1;
-        int p=0;
-        if(amount>=coins[i])
-            p = fun(amount-coins[i],coins,i,dp);
-        int np = fun(amount,coins,i-1,dp);
-        return dp[i][amount] = p + np;
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return fun(amount,coins,n-1,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1));
+        for(int j=0;j<=amount;j++)
+            dp[0][j] = (j%coins[0]==0);
+        for(int i=0;i<n;i++)
+            dp[i][0] = 1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int p=0;
+                if(j>=coins[i])
+                    p = dp[i][j-coins[i]];
+                int np = dp[i-1][j];
+                dp[i][j] = p+np;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
