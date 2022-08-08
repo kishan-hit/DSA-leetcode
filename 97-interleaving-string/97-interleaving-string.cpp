dbf3664cf173abd3d80999843089c5f2e45176d3
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-08-08 13:01:22 +0530

    Time: 3 ms (82.86%), Space: 6.3 MB (74.54%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    bool fun(string s1,string s2,string s3,int i,int j,int k,vector<vector<int>>&dp){
-        if(i==0 && j==0 && k==0)
-            return 1;
-        if(i>0 && j>0 && dp[i][j]!=-1)
-            return dp[i][j];
-        if(i>0 && j>0 && k>0 && (s1[i-1]==s3[k-1]) && (s2[j-1]==s3[k-1]))
-            return dp[i][j] = fun(s1,s2,s3,i-1,j,k-1,dp) || fun(s1,s2,s3,i,j-1,k-1,dp);
-        else if(i>0 && k>0 && s3[k-1]==s1[i-1])
-            return fun(s1,s2,s3,i-1,j,k-1,dp);
-        else if(j>0 && k>0 && s3[k-1]==s2[j-1])
-            return fun(s1,s2,s3,i,j-1,k-1,dp);
-        return 0;
-    }
     bool isInterleave(string s1, string s2, string s3) {
         int n1=s1.length(),n2=s2.length(),n3=s3.length();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        return fun(s1,s2,s3,n1,n2,n3,dp);
+        if(n3!=(n1+n2))
+            return false;
+        vector<int> prev(n2+1),cur(n2+1);
+        for(int i=n1;i>=0;i--){
+            for(int j=n2;j>=0;j--){
+                int k=i+j;
+                if(i==n1 && j==n2)
+                    cur[j] = 1;
+                else if(s1[i]==s3[k] && s2[j]==s3[k])
+                    cur[j] = prev[j] || cur[j+1];
+                else if(s1[i]==s3[k])
+                    cur[j] = prev[j];
+                else if(s2[j]==s3[k])
+                    cur[j] = cur[j+1];
+                else
+                    cur[j] = 0;
+            }
+            prev = cur;
+        }
+        return cur[0];
     }
 };
