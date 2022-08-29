df4a34e8264ab138a045d957941d01e5bc39f392
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-08-29 23:35:13 +0530

    Time: 68 ms (32.16%), Space: 12.3 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,9 +3,9 @@ public:
     void dfs(vector<vector<char>>&grid,int i,int j){
         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
             return;
-        if(grid[i][j]=='2' || grid[i][j]=='0')
+        if(grid[i][j]=='0')
             return;
-        grid[i][j] = '2';
+        grid[i][j] = '0';
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
         dfs(grid,i,j+1);
