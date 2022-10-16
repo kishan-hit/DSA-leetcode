b44252e87533ceb923b78c3ad956115aea7df185
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-10-16 22:34:23 +0530

    Added solution - LeetHub

---
+++ K closest elements - GFG/k-closest-elements.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    static bool cmp(pair<int,int>&a,pair<int,int>&b){
+        if(a.first==b.first)
+            return a.second>b.second;
+        return a.first<b.first;
+    } 
+    vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
+        // code here
+        vector<pair<int,int>> vec;
+        for(int i=0;i<n;i++)
+            if(arr[i]!=x)
+                vec.push_back({abs(x-arr[i]),arr[i]});
+        sort(vec.begin(),vec.end(),cmp);
+        vector<int> v;
+        for(int i=0;i<k;i++)
+            v.push_back(vec[i].second);
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, x;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> k >> x;
+        Solution ob;
+        auto ans = ob.printKClosest(arr, n, k, x);
+        for (auto e : ans) {
+            cout << e << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
