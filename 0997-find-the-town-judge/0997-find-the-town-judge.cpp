3275f68c13627a7cec89aa02b912e48bb1ac8dcb
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2023-01-23 23:30:51 +0530

    Time: 439 ms (17.60%), Space: 61.5 MB (65.98%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -3,15 +3,13 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(trust.size()==0 && n==1)
             return n;
-        unordered_map<int,int> mp1;
-        unordered_map<int,int> mp2;
+        unordered_map<int,int> mp;
         for(int i=0;i<trust.size();i++){
-            mp1[trust[i][0]]++;
-            mp2[trust[i][1]]++;
+            --mp[trust[i][0]];
+            ++mp[trust[i][1]];
         }
-        int sz = mp1.size();
-        for(auto it:mp2){
-            if(sz==n-1 && it.second==sz && mp1.find(it.first)==mp1.end())
+        for(auto it:mp){
+            if(it.second==n-1)
                 return it.first;
         }
         return -1;
