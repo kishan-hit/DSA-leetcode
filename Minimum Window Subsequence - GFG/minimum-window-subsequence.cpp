82359eff6579231ffdf779d8395aa4507253d104
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2023-02-10 19:42:11 +0530

    Added solution - LeetHub

---
+++ Minimum Window Subsequence - GFG/minimum-window-subsequence.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string minWindow(string str1, string str2) {
+        // Write your Code here
+        string str="";
+        int j=0,mini=INT_MAX;
+        for(int i=0;i<str1.length();i++){
+            if(str1[i]==str2[j]){
+                ++j;
+                if(j==str2.length()){
+                    int end=i;
+                    --j;
+                    while(j>=0){
+                        if(str1[i]==str2[j]){
+                            --j;
+                        }
+                        --i;
+                    }
+                    ++i;
+                    ++j;
+                    if(mini > (end-i+1)){
+                        mini = end-i+1;
+                        str = str1.substr(i,mini);
+                    }
+                }
+            }
+        }
+        return str;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string str1, str2;
+        cin >> str1 >> str2;
+
+        Solution obj;
+        cout << obj.minWindow(str1, str2) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
