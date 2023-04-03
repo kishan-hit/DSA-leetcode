c1121581c0b02da69f1508a4a0d2c807bf613932
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2023-04-03 18:09:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (74.96%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        unordered_map<char,int> mp;
+        vector<bool> vis(256,false);
+        for(auto it:s)
+            mp[it]++;
+        string res;
+        res += s[0];
+        vis[s[0]] = true;
+        mp[s[0]]--;
+        if(mp[s[0]]==0)
+            mp.erase(s[0]);
+        for(int i=1;i<s.length();i++){
+            mp[s[i]]--;
+            if(mp[s[i]]==0)
+                mp.erase(s[i]);
+            if(vis[s[i]])
+                continue;
+            while(!res.length()==0 && s[i]<res.back() && mp.find(res.back())!=mp.end()){
+                vis[res.back()] = false;
+                res.pop_back();
+            }
+            res += s[i];
+            vis[s[i]] = true;
+        }
+        return res;
+    }
+};
