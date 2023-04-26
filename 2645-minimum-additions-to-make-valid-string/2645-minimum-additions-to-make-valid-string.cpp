e865ecc8d9c7a749fe203c6f6b3321d07ccaa963
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2023-04-27 02:34:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (38.16%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int addMinimum(string word) {
-        int res=0,cnt=1,i=0;
-        for(i=0;i<word.length()-1;i++){
-            if(word[i]<word[i+1])
+        int res=0;
+        for(int i=0;i<word.length();){
+            int cnt=0;
+            if(word[i]=='a'){
                 ++cnt;
-            else{
-                res += (3-cnt);
-                cnt = 1;
+                ++i;
             }
-        }
-        if(word[i]=='a')
-            res += 2;
-        else if(word[i]=='b'){
-            if(!(i-1>=0 && word[i-1]=='a'))
-                res += 2;
-            else
-                res += 1;
-        }
-        else{
-            if(!(i-2>=0 && word[i-2]=='a' && word[i-1]=='b')){
-                if(!(i-1>=0 && word[i-1]=='b') && (!(i-1>=0 && word[i-1]=='a')))
-                    res += 2;
-                else
-                    res += 1;
+            if(word[i]=='b'){
+                ++cnt;
+                ++i;
             }
+            if(word[i]=='c'){
+                ++cnt;
+                ++i;
+            }
+            res += (3-cnt);
         }
         return res;
     }
